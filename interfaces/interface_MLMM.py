8e702f5e78037ccdca531e3b72816628e22ec962
Lester Hedges <lester.hedges@gmail.com>
2022-09-23 14:50:25 +0100

    Add license header to ML/MM interface.

--- interfaces/interface_MLMM.py
+++ interfaces/interface_MLMM.py
@@ -1,5 +1,24 @@
-# Adapted from code written by Kirill Zinovjev:
-# https://github.com/emedio/embedding
+######################################################################
+# ML/MM: https://github.com/emedio/embedding
+#
+# Copyright: 2022
+#
+# Authors: Kirill Zinovjev <kzinovjev@gmail.com>
+#          Lester Hedges   <lester.hedges@gmail.com>
+#
+# ML/MM is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# ML/MM is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ML/MM. If not, see <http://www.gnu.org/licenses/>.
+######################################################################
 
 import os
 import numpy as np
